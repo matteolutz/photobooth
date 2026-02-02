d0404b3e0b1b4cb0604a40f99280d0c1308967b2
Matteo Lutz <matteo.lutz06@outlook.de>
2026-02-02 17:38:43 +0100

    fix: conditional linkage based on platform

--- edsdk/build.rs
+++ edsdk/build.rs
@@ -3,6 +3,10 @@ fn main() {
         "cargo:rustc-link-search=framework={}",
         "/Library/Frameworks"
     );
-    println!("cargo:rustc-link-lib=framework=EDSDK");
-    println!("cargo:rustc-link-arg=-Wl,-rpath,/Library/Frameworks");
+
+    #[cfg(target_os = "macos")]
+    {
+        println!("cargo:rustc-link-lib=framework=EDSDK");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,/Library/Frameworks")
+    }
 }
